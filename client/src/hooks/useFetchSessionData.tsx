a94e6ba2fcb9e584acfd1df39d199165b3ee81ac
K3ndev <racelisjk@gmail.com>
2024-01-15 10:14:05 +0800

    fix: linting error in useEffect dep

--- client/src/hooks/useFetchSessionData.tsx
+++ client/src/hooks/useFetchSessionData.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
 import { fetchResultType } from './type'
 
+// todo when unmounted make sure the fetching fn is stop
 export const useFetchSessionData = () => {
     const [data, setData] = useState<fetchResultType>();
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
 
-    const abortController = new AbortController();
+    // const abortController = new AbortController();
 
     const fetchSession = async () => {
         const URL = '/api/session'
 
-        const res = await fetch(URL, {
-            signal: abortController.signal,
-        });
+        // const res = await fetch(URL, {
+        //     signal: abortController.signal,
+        // });
+
+        const res = await fetch(URL);
 
         setIsLoading(true);
 
@@ -36,6 +39,7 @@ export const useFetchSessionData = () => {
         // this return fn run... does after using the hooks does it unmounted ?
         // return () => abortController.abort();
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return { data, isLoading, isError };
